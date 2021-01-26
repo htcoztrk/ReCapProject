3e1ea229fac54f319d72940ea8149fe808758a09
htcoztrk <haticeozturk0207@gmail.com>
2021-01-26 18:41:41 +0300

    first commit

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> myList = new Dictionary<int,string>();
+            myList.Add(1, "H");
+            myList.Add(2, "M");
+           
+            foreach(KeyValuePair<int,string> person in myList)
+            {
+                Console.WriteLine("{0}.person is {1}",person.Key,person.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
